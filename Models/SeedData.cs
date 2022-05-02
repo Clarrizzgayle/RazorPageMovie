9bbee23d1dbdbc09c6b09072ed7d605dbde87ed6
TECH4ED-PC6 <TECH4ED-PC6@TECH4ED_6>
2022-05-02 11:08:13 +0800

    Add project files.

---
+++ Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+
+        {
+            using (var context = new RazorPageMovieContext(serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext>>()))
+            {
+                if (context == null || context.movie == null)
+                {
+                    throw new ArgumentNullException("null RazorPageMovieContext");
+                }
+                if (context.movie.Any())
+                {
+                    return;
+                }
+                context.movie.AddRange(
+                    new movie
+                    {
+                        Title = "Doremon",
+                        DateRelease = DateTime.Parse("1996-4-03"),
+                        Genre = "Kids",
+                        Price = 4.99M
+
+                    },
+                    new movie
+                    {
+                        Title = "Power Ranger",
+                        DateRelease = DateTime.Parse("1996-10-09"),
+                        Genre = "Kids",
+                        Price = 10.9M
+
+                    },
+                    new movie
+                    {
+                        Title = "Shaman King",
+                        DateRelease = DateTime.Parse("1996-8-03"),
+                        Genre = "anime",
+                        Price = 54M
+
+                    }
+                    );
+                context.SaveChanges();
+
+            }
+
+        }
+    }
+}
---
+++ Models/movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageMovie.Models
+
+{
+    public class movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }= String.Empty;    
+        
+        [DataType(DataType.Date)]
+        public DateTime DateRelease { get; set; }
+        public string Genre { get; set; }= String.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }  
+    }
+}
